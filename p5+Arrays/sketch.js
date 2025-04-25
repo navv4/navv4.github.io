29e2ad2fbd50326fc7f7efa0dbc7e701773581ce
navv4 <naveenap@ocadu.ca>
2025-04-25 13:12:21 -0400

    text and references

--- p5+Arrays/sketch.js
+++ p5+Arrays/sketch.js
@@ -11,17 +11,24 @@ fruits.push("orange", "guava", "mango", "cherry");
 console.log(fruits);
 
 
-for (let i = 0; i < fruits.length; i++) {
-    console.log(`Fruit ${i + 1}: ${fruits[i]}`);
-}
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(`Fruit ${i + 1}: ${fruits[i]}`);
+// }
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-
+    textSize(24);
+    fill(0);
 }
 
 
 function draw(){
     background('pink');
 
+    for (let i = 0; i < fruits.length; i++) {
+        text(`${i + 1}. ${fruits[i]}`, 50, 50 + i * 30);
+    }
 }
+
+//Tutorials remixed from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+// and: https://p5js.org/reference/p5/for/
